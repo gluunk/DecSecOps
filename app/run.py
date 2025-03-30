c80000e4c86d9e0b9476e90206dc96d9709bc746
gluunk <gluunk@gmail.com>
2025-03-30 17:39:57 -0400

    Começando minha pipeline

--- app/run.py
+++ app/run.py
@@ -1,4 +1,18 @@
+from prometheus_client import Counter, generate_latest
+from flask import Response
 from todo_project import app
 
+# Criando um contador para monitorar o número de requisições
+REQUEST_COUNT = Counter('request_count', 'Total de requisições recebidas')
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
+
+# Rota para expor as métricas em /metrics
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
